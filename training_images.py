918065978d0fff94704fa30498dfe6acc37c536a
Lukas <boehm-lukas@outlook.de>
2021-08-16 22:58:43 +0200

    Update training_images.py

--- training_images.py
+++ training_images.py
@@ -1,7 +1,20 @@
 import discord
+from dotenv import load_dotenv
+import os
 
-counter = 0
+load_dotenv()
+token = os.getenv('DISCORD_API_KEY')
+client = discord.Client()
 
-async for message in channel.history(limit=200):
-    if message.author == client.user:
-        counter += 1
+
+async def start():
+    await client.start(token)
+
+
+async def last_images():
+    channel = client.get_channel(642911304876818453)
+    for message in channel.history(limit=20):
+        print(message)
+
+start()
+last_images()
