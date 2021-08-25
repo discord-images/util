7fd75f074a83cb7d8584597f41a7406201f6611e
Lukas Böhm <boehm-lukas@outlook.de>
2021-08-25 19:48:21 +0200

    :construction: running script (kind of)

--- training_images.py
+++ training_images.py
@@ -1,20 +1,22 @@
 import discord
 from dotenv import load_dotenv
 import os
+import sys
 
 load_dotenv()
 token = os.getenv('DISCORD_API_KEY')
 client = discord.Client()
 
 
-async def start():
-    await client.start(token)
-
-
-async def last_images():
-    channel = client.get_channel(642911304876818453)
-    for message in channel.history(limit=20):
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    channel: discord.channel.TextChannel = client.get_channel(
+        642911304876818456)
+    if channel is None:
+        sys.exit()
+    for message in channel.history(limit=20).flatten():
         print(message)
+    sys.exit()
 
-start()
-last_images()
+client.run(token)
