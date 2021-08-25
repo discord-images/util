b24ca12a04dbc9e462cf68204dcdb180d836db88
Lukas <boehm-lukas@outlook.de>
2021-08-25 20:23:18 +0200

    :sparkles: working script

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # util
+
 utilities for improving discord images
 
 ## Setup
+
+Env Variables:
+
+- `DISCORD_API_KEY`: discord api key
+- `MESSAGE_LIMIT`: number of message the bot should go through to find training images
+- `DISCORD_CHANNEL_ID`: channel id the bot searches through
+
+## Run
 
 ```
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
+python training_images.py
 ```
-
--- training_images.py
+++ training_images.py
@@ -1,22 +1,37 @@
 import discord
 from dotenv import load_dotenv
 import os
-import sys
 
 load_dotenv()
 token = os.getenv('DISCORD_API_KEY')
+limit = int(os.getenv('MESSAGE_LIMIT'))
+channel_id = int(os.getenv('DISCORD_CHANNEL_ID'))
 client = discord.Client()
+
+
+async def shutdown():
+    await client.close()
+    try:
+        exit()
+    except:
+        print("closed")
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    channel: discord.channel.TextChannel = client.get_channel(
-        642911304876818456)
+    channel: discord.channel.TextChannel = client.get_channel(channel_id)
     if channel is None:
-        sys.exit()
-    for message in channel.history(limit=20).flatten():
-        print(message)
-    sys.exit()
+        await shutdown()
+    message: discord.Message
+
+    async for message in channel.history(limit=limit):
+        if len(message.attachments) > 0:
+            att: discord.Attachment
+            for att in message.attachments:
+                print(att.url)
+
+    await shutdown()
+
 
 client.run(token)
